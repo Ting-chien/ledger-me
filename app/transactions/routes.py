854277e13673af6a8aeee89395c01a84353723e0
ting-chien <eric821210@gmail.com>
2025-10-24 15:49:28 +0800

    Add table in transaction.index page

--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template
+from app.transactions.models import Transaction
 
 from . import blueprint
 
 
 @blueprint.route("/", methods=["GET"])
 def index():
-    return render_template("transactions/index.html")
+    transactions = Transaction.query.order_by(Transaction.created_at.desc()).all()
+    return render_template("transactions/index.html", transactions=transactions)
