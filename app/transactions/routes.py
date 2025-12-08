6a428413dfbf7aae51b657d2cf33651b0f65ca5d
ting-chien <eric821210@gmail.com>
2025-12-08 21:39:54 +0800

    Add new input paramters on transaction.index page and reset default value

--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -48,7 +48,7 @@ def index():
 
     # Pagination
     page = request.args.get('page', 1, type=int)
-    page_size = request.args.get('page_size', 5, type=int)
+    page_size = request.args.get('page_size', 10, type=int)
     pagination = query.order_by(Transaction.created_at.desc()).paginate(
         page=page, 
         per_page=page_size, 
